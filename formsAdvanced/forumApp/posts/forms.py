9b3a3c8612884c9d2a339dee33e81f4b200c72e7
angeluzunov10 <angel_d_u@abv.bg>
2024-10-16 13:44:35 +0300

    Added mixin for disabling all fields in deleting page, added some error messages, added condition for not including title in content.

--- formsAdvanced/forumApp/posts/forms.py
+++ formsAdvanced/forumApp/posts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
+from forumApp.posts.mixins import DisableFieldsMixin
 from forumApp.posts.models import Post
 
 
@@ -7,6 +9,42 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
+
+        error_messages = {
+            'title': {
+                'required': 'Please enter a title of your post.',
+                'max_length': f'The title is too long. Please keep it under {Post.TITLE_MAX_LENGTH} characters.'
+            }
+        }
+
+    def clean_author(self):
+        author = self.cleaned_data.get('author')
+
+        if author[0] != author[0].upper():
+            raise ValidationError('Author name must start with a capital letter!')
+
+        return author
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        title = cleaned_data.get('title')
+        content = cleaned_data.get('content')
+
+        if title and content and title in content:
+            raise ValidationError('The post title cannot be included in the post content!')
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        post = super().save(commit=False)
+
+        post.title = post.title.capitalize()
+
+        if commit:
+            post.save()
+
+        return post
 
 
 class PostCreateForm(PostForm):
@@ -17,19 +55,15 @@ class PostEditForm(PostForm):
     pass
 
 
-class PostDeleteForm(PostForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        for field in self.fields:
-            self.fields[field].disabled = True
+class PostDeleteForm(PostForm, DisableFieldsMixin):
+    disabled_fields = ('__all__',)
 
 
 class SearchForm(forms.Form):
     query = forms.CharField(
         label='',
         required=False,
-        max_length=100,
+        max_length=5,
         widget=forms.TextInput(
             attrs={
                 'placeholder': 'Search for a specific post...',
